98ac80da2814ecb151ca41a0d0397bd567dbb940
Shogo Wada <shogowada@users.noreply.github.com>
2016-12-23 05:43:42 -0600

    Create an empty client API

--- shared/src/main/scala/io/github/shogowada/scala/jsonrpc/client/JsonRpcClient.scala
+++ shared/src/main/scala/io/github/shogowada/scala/jsonrpc/client/JsonRpcClient.scala
@@ -10,21 +10,54 @@ class JsonRpcClient[JSON_SERIALIZER <: JsonSerializer]
 (
     val jsonSerializer: JSON_SERIALIZER
 ) {
-  def createApi[API](api: API): API = macro JsonRpcClientMacro.createApi[API]
+  def createApi[API]: API = macro JsonRpcClientMacro.createApi[API]
+}
+
+object JsonRpcClient {
+  def apply[JSON_SERIALIZER <: JsonSerializer](jsonSerializer: JSON_SERIALIZER) = new JsonRpcClient(jsonSerializer)
 }
 
 object JsonRpcClientMacro {
   def createApi[API: c.WeakTypeTag]
   (c: blackbox.Context)
-  (api: c.Expr[API])
   : c.Expr[API] = {
     import c.universe._
     val apiType: Type = weakTypeOf[API]
-    val apiMethods = MacroUtils[c.type](c).getApiMethods(apiType)
+    val memberFunctions = createMemberFunctions[c.type, API](c)
+    c.Expr[API](
+      q"""
+          new {} with $apiType {
+            ..$memberFunctions
+          }
+          """
+    )
+  }
+
+  private def createMemberFunctions[CONTEXT <: blackbox.Context, API: c.WeakTypeTag]
+  (c: CONTEXT)
+  : Iterable[c.Tree] = {
+    import c.universe._
+    val apiType: Type = weakTypeOf[API]
+    MacroUtils[c.type](c).getApiMethods(apiType)
+        .map((apiMethod: MethodSymbol) => createMemberFunction[c.type](c)(apiMethod))
+  }
+
+  private def createMemberFunction[CONTEXT <: blackbox.Context]
+  (c: CONTEXT)
+  (apiMethod: c.universe.MethodSymbol)
+  : c.Tree = {
+    import c.universe._
+    val parameterLists: List[List[Tree]] =
+      apiMethod.paramLists.map((parameterList: List[Symbol]) => {
+        parameterList.map((parameter: Symbol) => {
+          q"${parameter.name.toTermName}: ${parameter.typeSignature}"
+        })
+      })
+    val returnType: Type = apiMethod.returnType
     q"""
-        new $apiType {
+        override def ${apiMethod.name}(...$parameterLists): $returnType = {
+          throw new UnsupportedOperationException()
         }
         """
-    c.Expr[API](q"")
   }
 }
--- shared/src/test/scala/io/github/shogowada/scala/jsonrpc/client/JsonRpcClientTest.scala
+++ shared/src/test/scala/io/github/shogowada/scala/jsonrpc/client/JsonRpcClientTest.scala
@@ -1,5 +1,27 @@
 package io.github.shogowada.scala.jsonrpc.client
 
-class JsonRpcClientTest {
+import io.github.shogowada.scala.jsonrpc.serializers.UpickleJsonSerializer
+import org.scalatest.{Matchers, path}
 
+import scala.concurrent.Future
+
+class JsonRpcClientTest extends path.FunSpec
+    with Matchers {
+  override def newInstance: path.FunSpecLike = new JsonRpcClientTest
+
+  val target = JsonRpcClient(UpickleJsonSerializer())
+
+  describe("givem I have an API") {
+    trait Api {
+      def foo(bar: String, baz: Int): Future[String]
+    }
+
+    describe("when I create a client API") {
+      val api = target.createApi[Api]
+
+      it("then it should be an instance of the API") {
+        api.isInstanceOf[Api] should equal(true)
+      }
+    }
+  }
 }
