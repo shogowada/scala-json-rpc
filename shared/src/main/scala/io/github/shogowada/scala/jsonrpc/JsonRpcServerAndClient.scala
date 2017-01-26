e81125fc5f43929e47f425a07e34d4761d34b066
Shogo Wada <shogowada@users.noreply.github.com>
2017-01-26 05:58:47 -0600

    Return success status from receiveAndSend

--- shared/src/main/scala/io/github/shogowada/scala/jsonrpc/JsonRpcServerAndClient.scala
+++ shared/src/main/scala/io/github/shogowada/scala/jsonrpc/JsonRpcServerAndClient.scala
@@ -5,7 +5,7 @@ import io.github.shogowada.scala.jsonrpc.serializers.JsonSerializer
 import io.github.shogowada.scala.jsonrpc.server.{JsonRpcServer, JsonRpcServerMacro}
 import io.github.shogowada.scala.jsonrpc.utils.JsonRpcMacroUtils
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
@@ -13,13 +13,13 @@ class JsonRpcServerAndClient[JSON_SERIALIZER <: JsonSerializer](
     val server: JsonRpcServer[JSON_SERIALIZER],
     val client: JsonRpcClient[JSON_SERIALIZER]
 ) {
-  def send(json: String): Unit = client.send(json)
+  def send(json: String): Future[Option[String]] = client.send(json)
 
   def bindApi[API](api: API): Unit = macro JsonRpcServerAndClientMacro.bindApi[API]
 
   def createApi[API]: API = macro JsonRpcServerAndClientMacro.createApi[API]
 
-  def receiveAndSend(json: String): Unit = macro JsonRpcServerAndClientMacro.receiveAndSend
+  def receiveAndSend(json: String): Future[Unit] = macro JsonRpcServerAndClientMacro.receiveAndSend
 }
 
 object JsonRpcServerAndClient {
@@ -60,25 +60,39 @@ object JsonRpcServerAndClientMacro {
     )
   }
 
-  def receiveAndSend(c: blackbox.Context)(json: c.Expr[String]): c.Expr[Unit] = {
+  def receiveAndSend(c: blackbox.Context)(json: c.Expr[String]): c.Expr[Future[Unit]] = {
     import c.universe._
     val macroUtils = JsonRpcMacroUtils[c.type](c)
     val (serverAndClientDefinition, serverAndClient) = macroUtils.prefixDefinitionAndReference
     val server: Tree = q"$serverAndClient.server"
     val client: Tree = q"$serverAndClient.client"
     val executionContext: c.Expr[ExecutionContext] = c.Expr(q"$server.executionContext")
-    c.Expr[Unit](
+    c.Expr[Future[Unit]](
       q"""
           ..${macroUtils.imports}
           $serverAndClientDefinition
-          val wasJsonRpcResponse: Boolean = $client.receive($json)
-          if (!wasJsonRpcResponse) {
-            $server.receive($json)
-              .onComplete((tried: Try[Option[String]]) => tried match {
-                case Success(Some(responseJson: String)) => $client.send(responseJson)
-                case _ =>
-              })($executionContext)
+          def receiveAndSend(json: String): Future[Unit] = {
+            val wasJsonRpcResponse: Boolean = $client.receive(json)
+            if (!wasJsonRpcResponse) {
+              $server.receive(json)
+                .flatMap((maybeResponseJsonFromUs: Option[String]) => {
+                  maybeResponseJsonFromUs match {
+                    case Some(responseJsonFromUs) => $client.send(responseJsonFromUs)
+                    case None => Future(None)($executionContext)
+                  }
+                })($executionContext)
+                .flatMap((maybeResponseJsonFromThem: Option[String]) => {
+                  maybeResponseJsonFromThem match {
+                    case Some(responseJsonFromThem) => receiveAndSend(responseJsonFromThem)
+                    case None => Future(None)($executionContext)
+                  }
+                })($executionContext)
+                .map(_ => ())
+            } else {
+              Future()($executionContext)
+            }
           }
+          receiveAndSend($json)
           """
     )
   }
