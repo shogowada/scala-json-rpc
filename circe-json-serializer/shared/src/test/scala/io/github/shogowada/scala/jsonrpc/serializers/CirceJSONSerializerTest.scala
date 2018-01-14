53aca314230374576d4a165e25b388f642854630
Shogo Wada <shogowada@users.noreply.github.com>
2018-01-14 10:16:36 -0600

    Depend on circe-generic

--- circe-json-serializer/shared/src/test/scala/io/github/shogowada/scala/jsonrpc/serializers/CirceJSONSerializerTest.scala
+++ circe-json-serializer/shared/src/test/scala/io/github/shogowada/scala/jsonrpc/serializers/CirceJSONSerializerTest.scala
@@ -1,28 +1,23 @@
 package io.github.shogowada.scala.jsonrpc.serializers
 
-import io.circe.generic.extras.Configuration
-import org.scalatest.{OneInstancePerTest, Matchers, FreeSpec}
+import org.scalatest.{FreeSpec, Matchers, OneInstancePerTest}
 
 class CirceJSONSerializerTest extends FreeSpec
     with OneInstancePerTest
-    with Matchers
-{
+    with Matchers {
   override def newInstance = new CirceJSONSerializerTest
 
   val jsonSerializer = CirceJSONSerializer()
 
-  "given I have automatic derivation, and snake-case keys configured" - {
-    import io.circe.generic.extras.auto._
-    implicit val conf = Configuration.default.withSnakeCaseKeys
-
+  "given I have a plain case class" - {
     case class Test(helloWorld: String)
     val expectedDeserialized = Test("hello world")
-    val expectedSerialized = """{"hello_world":"hello world"}"""
+    val expectedSerialized = """{"helloWorld":"hello world"}"""
 
     s"when I serialize $expectedDeserialized" - {
       val serialized = jsonSerializer.serialize(expectedDeserialized)
 
-      "then it should serialize as expected" in {
+      "then it should serialize into JSON" in {
         serialized should equal(Some(expectedSerialized))
       }
     }
@@ -30,7 +25,7 @@ class CirceJSONSerializerTest extends FreeSpec
     s"when I deserialize $expectedSerialized" - {
       val deserialized = jsonSerializer.deserialize[Test](expectedSerialized)
 
-      "then it should deserialize as expected" in {
+      "then it should deserialize into the case class" in {
         deserialized should equal(Some(expectedDeserialized))
       }
     }
